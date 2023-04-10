17f0ede69598dc1c7c3687f8213f73d4a9c912a1
Dan Suzor <24298908+dls4523@users.noreply.github.com>
2023-04-10 22:06:41 +0000

    update

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-	printf("hello, world\n");
+	string name = get_string("What is your name? ");
+	printf("hello, %s\n", name);
+	int number = get_int("Please enter your favorite number: ");
+	if(number < 50)
+	{	
+		printf("Your favorite number is less than 50...\n");
+	}
+	else
+	{
+		printf("Your favorite number is greater than or equal to 50!\n");
+	}
 	return 0;
 }
 
